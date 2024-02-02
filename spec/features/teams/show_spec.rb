b857eddc2445b75d400299d5c8b4dae13fa2cfe2
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-02 18:58:42 -0500

    test: feature test for count of players

--- spec/features/teams/show_spec.rb
+++ spec/features/teams/show_spec.rb
@@ -21,4 +21,19 @@ RSpec.describe "Team index page", type: :feature do
 
       expect(current_path).to eq("/teams/#{man_city.id}/players")
    end
+
+# User Story 7, Parent Child Count
+
+# As a visitor
+# When I visit a parent's show page
+# I see a count of the number of children associated with this parent
+   it 'shows the count of players associated to that team' do
+      man_city = Team.create!(name: "Manchester City FC", state: "Manchester", champ_position: 1, hiring_players: false)
+      ederson = man_city.players.create!(name: "Ederson", position: "Goalkeeper", jersey_number: 31, injuried: false)
+      de_bruyne = man_city.players.create!(name: "Kevin De Bruyne", position: "Midfielder", jersey_number: 17, injuried: false)
+
+      visit "/teams/#{man_city.id}"
+
+      expect(page).to have_content("Players: 2")
+   end
 end
