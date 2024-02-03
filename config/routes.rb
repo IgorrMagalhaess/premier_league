bf6c4b78b574e10aebd8eae44be9792008497183
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-03 15:33:25 -0500

    feat:route to patch

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
   get '/teams/:id', to: "teams#show"
   get '/teams/:id/players', to: "team_players#index"
   get '/teams/:id/edit', to: "teams#edit"
-  
+  patch '/teams/:id', to: "teams#update"
+
   get '/players', to: "players#index"
   get '/players/:id', to: "players#show"
 
