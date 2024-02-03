bbe7d4a4e2732650fbd254368a2c564036d35a1d
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-03 15:13:42 -0500

    test:edit

--- spec/features/teams/show_spec.rb
+++ spec/features/teams/show_spec.rb
@@ -74,4 +74,24 @@ RSpec.describe "Team index page", type: :feature do
       expect(page).to have_content(ederson.name)
       expect(page).to have_content(de_bruyne.name)
    end
+
+   # As a visitor
+   # When I visit a parent show page
+   # Then I see a link to update the parent "Update Parent"
+   # When I click the link "Update Parent"
+   # Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
+   # When I fill out the form with updated information
+   # And I click the button to submit the form
+   # Then a `PATCH` request is sent to '/parents/:id',
+   # the parent's info is updated,
+   # and I am redirected to the Parent's Show page where I see the parent's updated info
+   it 'has a link to update that team' do
+      man_city = Team.create!(name: "Manchester City", state: "Manchester", champ_position: 3, hiring_players: false)
+      ederson = man_city.players.create!(name: "Ederson", position: "Goalkeeper", jersey_number: 31, injuried: false)
+      de_bruyne = man_city.players.create!(name: "Kevin De Bruyne", position: "Midfielder", jersey_number: 17, injuried: false)
+
+      visit "/teams/#{man_city.id}"
+
+      expect(page).to have_button("Edit Team")
+   end
 end
