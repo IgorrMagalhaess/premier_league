b75ba9d7faeba63a89295cb6db37920358eaadd7
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-02 21:40:12 -0500

    test: features/players/index has a link to teams index

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -18,4 +18,19 @@ RSpec.describe 'the players show page', type: :feature do
 
       expect(page).to have_content(ederson.position)
    end
+
+   it 'has a link to the teams index' do
+      man_city = Team.create!(name: "Manchester City", state: "Manchester", champ_position: 3, hiring_players: false)
+      liverpool = Team.create!(name: "Liverpool FC", state: "Liverpool", champ_position: 5, hiring_players: true)
+      tottenham = Team.create!(name: "Tottenham Hotspur FC", state: "Tottenham", champ_position: 8, hiring_players: true)
+
+      visit "/players/#{ederson.id}"
+
+      click_on "Teams Index", match: :first
+
+      expect(page).to have_content("Teams Index")
+      expect(page).to have_content(man_city.name)
+      expect(page).to have_content(liverpool.name)
+      expect(page).to have_content(tottenham.name)
+   end
 end
