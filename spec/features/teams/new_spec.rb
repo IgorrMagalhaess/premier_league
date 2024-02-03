d6135d11127994cd8dd8df0af50b775545c1afb2
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-03 12:34:41 -0500

    fix

--- spec/features/teams/new_spec.rb
+++ spec/features/teams/new_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Teams new page", type: :feature do
       fill_in 'Name', with: 'Arsenal FC'
       fill_in 'State', with: 'Holloway'
       fill_in 'Position', with: 2
-      fill_in 'On the Market', with: true
+      fill_in 'On the Market', with: "true"
 
       click_button 'Submit'
 
