8261a7946cc0ac6ba26066f1e1ea4a8329a052c9
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-02-06 09:21:55 -0500

    test: robust testing for welcome index:

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -5,17 +5,29 @@ RSpec.describe "Premier League index page", type: :feature do
       visit "/"
 
       expect(page).to have_link 'Teams Index'
+
+      click_on "Teams Index", match: :first
+
+      expect(current_path).to eq("/teams")
    end
 
    it "can see a link to player index" do
       visit "/"
 
       expect(page).to have_link 'Players Index'
+
+      click_on "Players Index", match: :first
+
+      expect(current_path).to eq("/players")
    end
 
    it "can see a link to new team" do
       visit "/"
 
       expect(page).to have_link 'Add New Team'
+
+      click_on "Add New Team"
+
+      expect(current_path).to eq("/teams/new")
    end
 end
